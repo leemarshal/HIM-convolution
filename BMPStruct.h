b8532e23caf2a3ff8dbaf88141205e28cac23e84
gwlee <leemarshal@naver.com>
2023-11-08 04:59:24 +0900

    add convolution and move function to headerfile

--- BMPStruct.h
+++ BMPStruct.h
@@ -6,6 +6,9 @@
 #define HIM_CONVOLUTION_BMPSTRUCT_H
 
 #include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 #pragma pack(push, 1) // 정렬을 1바이트 단위로 설정
 
@@ -40,5 +43,77 @@ struct RGBTriple {
     uint8_t red;
 };
 #pragma pack(pop)     // 패딩 설정을 원래대로 돌려놓음
+
+using namespace std;
+
+void convertToGrayscale(ifstream &inFile, ofstream &outFile, BMPINFOHEADER &infoHeader) {
+    uint8_t pixel[3];
+    uint8_t gray;
+
+    for (int i = 0; i < infoHeader.height; ++i) {
+        for (int j = 0; j < infoHeader.width; ++j) {
+            inFile.read(reinterpret_cast<char*>(pixel), 3);
+            gray = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
+            //gray = 0.333 * pixel[2] + 0.333 * pixel[1] + 0.333 * pixel[0];
+            pixel[0] = pixel[1] = pixel[2] = gray;
+            outFile.write(reinterpret_cast<char*>(pixel), 3);
+        }
+    }
+}
+
+void flipImageHorizontallyAndVertically(ifstream &inFile, BMPINFOHEADER &infoHeader,
+                                        vector<vector<RGBTriple>> &flippedImage) {
+    flippedImage.assign(infoHeader.height, vector<RGBTriple>(infoHeader.width));
+    RGBTriple pixel;
+    int padding = (4 - (static_cast<int>(infoHeader.width * sizeof(RGBTriple)) % 4)) % 4;
+    for (int y = 0; y < infoHeader.height; y++) {
+        for (int x = 0; x < infoHeader.width; x++) {
+            inFile.read(reinterpret_cast<char*>(&pixel), sizeof(RGBTriple));
+            flippedImage[infoHeader.height - 1 - y][infoHeader.width - 1 - x] = pixel;
+        }
+        inFile.ignore(padding);
+    }
+}
+
+void loadBMPToVector(ifstream &inFile, BMPINFOHEADER &infoHeader,
+                     vector<vector<RGBTriple>> &Image) {
+    Image.assign(infoHeader.height, vector<RGBTriple>(infoHeader.width));
+    RGBTriple pixel;
+    int padding = (4 - (static_cast<int>(infoHeader.width * sizeof(RGBTriple)) % 4)) % 4;
+    for (int y = 0; y < infoHeader.height; y++) {
+        for (int x = 0; x < infoHeader.width; x++) {
+            inFile.read(reinterpret_cast<char*>(&pixel), sizeof(RGBTriple));
+            Image[y][x] = pixel;
+        }
+        inFile.ignore(padding);
+    }
+}
+
+void convolution(vector<vector<RGBTriple>> &small, vector<vector<RGBTriple>> &big,
+                 vector<vector<int>> &convolutionMap,
+                 BMPINFOHEADER smallInfoHeader, BMPINFOHEADER bigInfoHeader) {
+    int h = bigInfoHeader.height - smallInfoHeader.height + 1;
+    int w = bigInfoHeader.width - smallInfoHeader.width + 1;
+    convolutionMap.assign(h, vector<int>(w));
+    int maxValue = 0;
+    for(int i = 0; i < h; ++i) {
+        for(int j = 0; j < w; ++j) {
+            for(int k = 0; k < smallInfoHeader.height; ++k) {
+                for(int l = 0; l < smallInfoHeader.width; ++l) {
+                    convolutionMap[i][j] += small[k][l].blue * big[i + k][j + l].blue;
+                }
+            }
+            if (convolutionMap[i][j] > maxValue) {
+                maxValue = convolutionMap[i][j];
+            }
+        }
+    }
+    if(maxValue == 0) return;
+    for(int i = 0; i < h; ++i)
+        for(int j = 0; j < w; ++j)
+            convolutionMap[i][j] = static_cast<int>((static_cast<float>(convolutionMap[i][j]) / maxValue) * 255);
+}
+
+//box 치기 grid를 나눠서 local maximum을 찾아보자
 
 #endif //HIM_CONVOLUTION_BMPSTRUCT_H
